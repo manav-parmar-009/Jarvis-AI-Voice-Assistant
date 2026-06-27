de589ca1092b1fe2bdd75c96ff76eae692b3b201
manav-parmar-009 <parmarmanav499@gmail.com>
2026-06-27 23:34:20 +0530

    client.py

--- mega_project_1_jarvis/client.py
+++ mega_project_1_jarvis/client.py
@@ -1,9 +1,9 @@
 from google import genai
 
 client = genai.Client(
-    api_key="AQ.Ab8RN6Jb8qbEOQmSODUWOD5BV8zgZ0jmV3idU2tVkLtlG4WA"
+    api_key="AQ.Ab8RN6Jb8qEOQmSODUWODBV8zgZ0jmV3idU2tVkLtlG4WA"
 )
-# dont copy this api i have removed some words  write your own api :)
+
 
 def ask_gemini(prompt):
     try:
